db57c3eee6335c006ea1586355aa6d5871ed5347
sreina11 <54913455+sreina11@users.noreply.github.com>
2025-10-01 11:43:35 +0200

    main.py

--- main.py
+++ main.py
@@ -13,15 +13,45 @@ gc = gspread.service_account(filename='creds.json')
 sheet = gc.open("Copia de Telegram Elite").worksheet("MA")
 
 symbols = [
-    "SPY", "QQQ", "MSFT", "GOOGL", "META", "IBM", "V", "JPM", "MA", "AAPL", "AMD", "NVDA", "AMZN", "KO", "DIS",
-    "MCD", "NFLX", "CAT", "TSLA", "CVX", "XOM", "JNJ", "NKE"
+    # Índices
+    "SPX500", "DJI", "NAS100",
+    # Acciones
+    "SPY", "MSFT", "GOOGL", "META", "IBM", "V", "JPM", "MA", "AAPL", "AMD", "NVDA", "AMZN", "KO", "DIS",
+    "MCD", "NFLX", "CAT", "TSLA", "CVX", "XOM", "JNJ",
+    # Forex
+    "USDJPY", "USDCAD", "USDCHF", "USDAUD", "EURUSD", "EURJPY", "EURGBP", "EURAUD", "GBPUSD", "GBPJPY",
+    "AUDUSD", "AUDJPY", "CADJPY", "CHFJPY", "CADCHF",
+    # Cripto
+    "BTCUSD", "ETHUSD", "XRPUSDT", "BNBUSDT", "SOLUSDT", "AVAXUSDT", "XLMUSDT", "LINKUSDT", "PAXGUSDT",
+    # Commodities
+    "XAUUSD", "UKOIL"
 ]
 
 def obtener_exchange(symbol):
-    if symbol in {"SPY", "QQQ", "AAPL", "MSFT", "GOOGL", "META", "NVDA", "AMD", "AMZN", "NFLX", "TSLA"}:
+    if symbol in {"BTCUSD", "ETHUSD", "XRPUSDT", "BNBUSDT", "SOLUSDT", "AVAXUSDT", "XLMUSDT", "LINKUSDT", "PAXGUSDT"}:
+        return "BINANCE"
+    elif symbol in {"SPY", "AAPL", "MSFT", "GOOGL", "META", "NVDA", "AMD", "AMZN", "NFLX", "TSLA"}:
         return "NASDAQ"
-    else:
+    elif symbol in {"CVX", "XOM", "KO", "DIS", "MCD", "IBM", "V", "JPM", "MA", "CAT"}:
         return "NYSE"
+    elif symbol in {"UKOIL", "SPX500", "DJI", "NAS100"}:
+        return "TVC"
+    elif symbol in {"XAUUSD", "EURUSD", "USDJPY", "GBPUSD", "USDCAD", "USDCHF", "AUDUSD", "NZDUSD",
+                    "EURJPY", "EURGBP", "EURAUD", "GBPJPY", "AUDJPY", "CADJPY", "CHFJPY", "CADCHF"}:
+        return "OANDA"
+    else:
+        return "BINANCE"
+
+def obtener_screener(symbol):
+    if symbol in {"BTCUSD", "ETHUSD", "XRPUSDT", "BNBUSDT", "SOLUSDT", "AVAXUSDT", "XLMUSDT", "LINKUSDT", "PAXGUSDT"}:
+        return "crypto"
+    elif symbol in {"USDJPY", "USDCAD", "USDCHF", "USDAUD", "EURUSD", "EURJPY", "EURGBP", "EURAUD", "GBPUSD", "GBPJPY",
+                    "AUDUSD", "AUDJPY", "CADJPY", "CHFJPY", "CADCHF", "XAUUSD", "UKOIL"}:
+        return "forex"
+    elif symbol in {"SPX500", "DJI", "NAS100"}:
+        return "indices"
+    else:
+        return "america"
 
 # Encabezado
 sheet.update('A1:B1', [["Activo", "Precio actual"]])
@@ -30,11 +60,12 @@ sheet.update('A1:B1', [["Activo", "Precio actual"]])
 rows = []
 for symbol in symbols:
     exchange = obtener_exchange(symbol)
+    screener = obtener_screener(symbol)
     try:
         handler = TA_Handler(
             symbol=symbol,
             exchange=exchange,
-            screener="america",
+            screener=screener,
             interval=Interval.INTERVAL_4_HOURS
         )
         analysis = handler.get_analysis()
